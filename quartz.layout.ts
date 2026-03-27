3b5e3775e2b07581a06d441c90e7b6ee32dc764f
saisingh-droid <saisingh@ttionline.org>
2026-03-27 18:07:10 +0530

    Quartz sync: Mar 27, 2026, 6:07 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,7 +5,23 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [],
+  afterBody: [
+    Component.Comments({
+    provider: 'giscus',
+    options: {
+      // from data-repo
+      repo: 'saisingh-droid/data-entry-guide',
+      // from data-repo-id
+      repoId: 'R_kgDORtbKzQ',
+      // from data-category
+      category: 'Announcements',
+      // from data-category-id
+      categoryId: 'DIC_kwDORtbKzc4C5ZcD',
+      // from data-lang
+      lang: 'en'
+    }
+  }),
+  ],
   footer: Component.Footer({
     //<p>© {year} <strong>Sai Singh Parihar</strong> · Built with ❤️ · Maintained by <strong>Sai Singh Parihar</strong></p>
     links: {
